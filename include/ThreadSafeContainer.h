d53a7c623ed8743a00b0fd24dbbcb7f4e62557ec
Levente Batuska <mr.levente97@gmail.com>
2025-01-29 22:08:40 +0100

    feat(ThreadSafeContainer): add support for non shared mutex and FairRWLock

--- include/ThreadSafeContainer.h
+++ include/ThreadSafeContainer.h
@@ -1,13 +1,20 @@
 #pragma once
+#include "FairRWLock.h"
 #include <memory>
 #include <mutex>
 #include <shared_mutex>
 
-template <typename T> class ThreadSafeContainer {
+template <typename T, typename LockType = std::shared_mutex>
+class ThreadSafeContainer {
+  static_assert(std::is_same_v<LockType, std::shared_mutex> ||
+                    std::is_same_v<LockType, std::mutex> ||
+                    std::is_same_v<LockType, FairRWLock>,
+                "ThreadSafeContainer can only be used with std::shared_mutex, "
+                "std::mutex, or FairRWLock");
 
 private:
   std::shared_ptr<T> data;
-  std::shared_ptr<std::shared_mutex> rwLock;
+  std::shared_ptr<LockType> rwLock;
 
 public:
   ThreadSafeContainer(T &&initialData)
@@ -21,6 +28,60 @@ public:
   auto read(Func func) const
       -> decltype(func(std::declval<const T &>())) const {
     std::shared_lock lock(*rwLock);
+    return func(*data);
+  }
+
+  template <typename Func> auto write(Func func) -> decltype(func(*data)) {
+    std::unique_lock lock(*rwLock);
+    return func(*data);
+  }
+};
+
+template <typename T> class ThreadSafeContainer<T, FairRWLock> {
+private:
+  std::shared_ptr<T> data;
+  std::shared_ptr<FairRWLock> rwLock;
+
+public:
+  ThreadSafeContainer(T &&initialData)
+      : data(std::make_shared<T>(std::move(initialData))),
+        rwLock(std::make_shared<FairRWLock>()) {}
+
+  ThreadSafeContainer(const ThreadSafeContainer &other)
+      : data(other.data), rwLock(other.rwLock) {}
+
+  template <typename Func>
+  auto read(Func func) const -> decltype(func(std::declval<const T &>())) {
+    rwLock->acquire_read();
+    auto result = func(*data);
+    rwLock->release_read();
+    return result;
+  }
+
+  template <typename Func> auto write(Func func) -> decltype(func(*data)) {
+    rwLock->acquire_write();
+    auto result = func(*data);
+    rwLock->release_write();
+    return result;
+  }
+};
+
+template <typename T> class ThreadSafeContainer<T, std::mutex> {
+private:
+  std::shared_ptr<T> data;
+  std::shared_ptr<std::mutex> rwLock;
+
+public:
+  ThreadSafeContainer(T &&initialData)
+      : data(std::make_shared<T>(std::move(initialData))),
+        rwLock(std::make_shared<std::mutex>()) {}
+
+  ThreadSafeContainer(const ThreadSafeContainer &other)
+      : data(other.data), rwLock(other.rwLock) {}
+
+  template <typename Func>
+  auto read(Func func) const -> decltype(func(std::declval<const T &>())) {
+    std::unique_lock lock(*rwLock);
     return func(*data);
   }
 
