aa8827397e902e716b4bfb957ed198e9fd2869f2
Levente Batuska <mr.levente97@gmail.com>
2025-02-08 01:18:57 +0100

    feat(TaskScheduler): improve introspection, task handling, and lifecycle
    - Introduce basic introspection for metrics such as task start timestamps
    - Add `getThreadStartTimestamp` to track thread start times
    - Allow setting a callback for completed tasks with `setTaskDoneCallback` (passing thread ID)
    - Implement move constructor for better resource management
    - Inline `addTask` for efficiency
    - Add `getNumThreads` to retrieve thread count
    - Improve logic around queue closure

--- include/TaskScheduler.h
+++ include/TaskScheduler.h
@@ -3,6 +3,8 @@
 #include "SafeQueue.h"
 #include <atomic>
 #include <chrono>
+#include <cstddef>
+#include <cstdint>
 #include <functional>
 #include <iostream>
 #include <ostream>
@@ -16,19 +18,24 @@ private:
   std::vector<std::thread> workerThreads;
   std::atomic<bool> isRunning;
   const size_t numThreads;
+  std::vector<uint64_t> threadStartTimestamps;
+  std::function<void(size_t)> taskDoneCallback;
+  std::mutex callbackMutex;
 
-public:
-  TaskScheduler(size_t numThreads, size_t queueSize)
-      : taskQueue(queueSize), isRunning(true), numThreads(numThreads) {
-    for (size_t i = 0; i < numThreads; ++i) {
-      workerThreads.emplace_back([this]() { this->workerFunction(); });
-    }
-  }
-
-  void workerFunction() {
-    while (isRunning || !taskQueue.empty()) {
+  void workerFunction(size_t threadId) {
+    while (isRunning) {
+      if (taskQueue.empty()) {
+        taskQueue.waititem();
+        if (!isRunning && taskQueue.empty()) {
+          return;
+        }
+      }
       auto task = taskQueue.pop();
       if (task) {
+        threadStartTimestamps[threadId] =
+            std::chrono::duration_cast<std::chrono::seconds>(
+                std::chrono::system_clock::now().time_since_epoch())
+                .count();
         try {
           task();
         } catch (const std::exception &e) {
@@ -36,17 +43,35 @@ public:
         } catch (...) {
           std::cerr << "Caught unknown exception\n";
         }
+        std::lock_guard<std::mutex> lock(callbackMutex);
+        if (taskDoneCallback) {
+          taskDoneCallback(threadId);
+        }
       }
     }
   }
+
+public:
+  TaskScheduler(size_t numThreads, size_t queueSize)
+      : taskQueue(queueSize), isRunning(true), numThreads(numThreads) {
+    threadStartTimestamps.resize(numThreads);
+    for (size_t i = 0; i < numThreads; ++i) {
+      workerThreads.emplace_back([this, i]() { this->workerFunction(i); });
+    }
+  }
+
+  TaskScheduler(TaskScheduler &&other) noexcept
+      : taskQueue(std::move(other.taskQueue)), numThreads(other.numThreads) {};
 
   // template <typename Func> void addTask(Func &&task) {
   //   taskQueue.push(std::forward<Func>(task));
   // }
 
-  bool addTask(std::function<void()> &&task) {
+  inline bool addTask(std::function<void()> &&task) {
     return taskQueue.push(std::move(task));
   }
+
+  inline size_t getNumThreads() const { return numThreads; }
 
   void stop() {
     isRunning = false;
@@ -68,6 +93,20 @@ public:
     if (isRunning) {
       stop();
     }
+  }
+
+  // size_t is the threadId of the thread that finished with a task
+  // in the range [0 -> numThreads]
+  void setTaskDoneCallback(std::function<void(size_t)> callback) {
+    std::lock_guard<std::mutex> lock(callbackMutex);
+    taskDoneCallback = std::move(callback);
+  }
+
+  uint64_t getThreadStartTimestamp(size_t threadId) const {
+    if (threadId < numThreads) {
+      return threadStartTimestamps[threadId];
+    }
+    return 0;
   }
 };
 } // namespace cpputils
