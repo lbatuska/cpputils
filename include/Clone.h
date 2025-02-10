85e5111909ba694ce268b940048ba4af20794942
Levente Batuska <mr.levente97@gmail.com>
2025-02-10 14:49:29 +0100

    fix(Clone): add noexcept to clone method

--- include/Clone.h
+++ include/Clone.h
@@ -4,6 +4,6 @@
 namespace cpputils {
 template <typename T> class Clone {
 public:
-  virtual std::unique_ptr<T> clone() const = 0;
+  virtual std::unique_ptr<T> clone() const noexcept = 0;
 };
 } // namespace cpputils
