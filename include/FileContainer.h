865033c97850576aecdc127b1cec3d7db32301a7
Levente Batuska <mr.levente97@gmail.com>
2025-03-07 17:08:18 +0100

    feat(FileContainer): initial implementation

---
+++ include/FileContainer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <filesystem>
+#include <map>
+#include <mutex>
+#include <optional>
+#include <string>
+#include <string_view>
+
+namespace cpputils {
+class FileContainer {
+ private:
+  std::map<std::string, std::string> data;
+  const std::filesystem::path abs_path;
+  std::mutex mtx;
+
+  static std::string read_file(const std::filesystem::path& file_path);
+
+ public:
+  FileContainer(std::string_view folder)
+      : abs_path(std::filesystem::absolute(folder)) {}
+
+  // Returns cache or reads file then returns content
+  std::string GetFileContent(const std::string& filename);
+
+  // Updates cache either to the value of content or by reading the file
+  std::string SetFileContent(std::string filename,
+                             std::optional<std::string> content = std::nullopt);
+};
+}  // namespace cpputils
---
+++ src/FileContainer.cpp
@@ -0,0 +1,50 @@
+#include "FileContainer.h"
+
+#include <fstream>
+#include <sstream>
+
+namespace cpputils {
+
+std::string FileContainer::read_file(const std::filesystem::path& file_path) {
+  std::ifstream file(
+      file_path,
+      std::ios::in |
+          std::ios::binary);  // Open in binary mode to preserve all bytes
+  if (!file.is_open()) {
+    return "";
+  }
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  return buffer.str();
+}
+
+std::string FileContainer::GetFileContent(const std::string& filename) {
+  std::lock_guard<std::mutex> lock(mtx);
+  // Check cache first
+  auto it = data.find(filename);
+  if (it != data.end()) {
+    return it->second;
+  }
+
+  std::filesystem::path file_path = abs_path / filename;
+  std::string content = read_file(file_path);
+  data[filename] = content;
+  return content;
+}
+
+std::string FileContainer::SetFileContent(std::string filename,
+                                          std::optional<std::string> content) {
+  std::lock_guard<std::mutex> lock(mtx);
+  if (content) {
+    data[filename] = *content;
+    return *content;
+  }
+
+  std::filesystem::path file_path = abs_path / filename;
+  std::string file_content = read_file(file_path);
+  return data[filename] = file_content;
+  return file_content;
+}
+
+}  // namespace cpputils
